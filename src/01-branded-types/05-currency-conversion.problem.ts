9078fa8ada4f2f5a93900fd5a987bdeac5e4c4d8
Cesar Mejia <cmejia@gmail.com>
2023-03-22 15:28:57 +0700

    complete lesson 01 -07

--- src/01-branded-types/05-currency-conversion.problem.ts
+++ src/01-branded-types/05-currency-conversion.problem.ts
@@ -1,49 +1,47 @@
-import { describe, it } from "vitest";
-import { Brand } from "../helpers/Brand";
+import { describe, it } from 'vitest'
+import { Brand } from '../helpers/Brand'
 
 interface User {
-  id: string;
-  name: string;
-  maxConversionAmount: number;
+  id: string
+  name: string
+  maxConversionAmount: number
 }
+
+type ConvertedAmount = Brand<number, 'ConvertedAmount'>
+type AuthorizedUser = Brand<User, 'AuthorizedUser'>
 
 // Mocks a function that uses an API to convert
 // One currency to another
-const getConversionRateFromApi = async (
-  amount: number,
-  from: string,
-  to: string,
-) => {
-  return Promise.resolve(amount * 0.82);
-};
-
+const getConversionRateFromApi = async (amount: number, from: string, to: string) => {
+  return Promise.resolve((amount * 0.82) as ConvertedAmount)
+}
 // Mocks a function which actually performs the conversion
-const performConversion = async (user: User, to: string, amount: number) => {};
+const performConversion = async (user: AuthorizedUser, to: string, amount: ConvertedAmount) => {}
 
-const ensureUserCanConvert = (user: User, amount: number): User => {
+const ensureUserCanConvert = (user: User, amount: ConvertedAmount) => {
   if (user.maxConversionAmount < amount) {
-    throw new Error("User cannot convert currency");
+    throw new Error('User cannot convert currency')
   }
 
-  return user;
-};
+  return user as AuthorizedUser
+}
 
-describe("Possible implementations", () => {
-  it("Should error if you do not authorize the user first", () => {
+describe('Possible implementations', () => {
+  it('Should error if you do not authorize the user first', () => {
     const handleConversionRequest = async (
       user: User,
       from: string,
       to: string,
       amount: number,
     ) => {
-      const convertedAmount = await getConversionRateFromApi(amount, from, to);
+      const convertedAmount = await getConversionRateFromApi(amount, from, to)
 
       // @ts-expect-error
-      await performConversion(user, to, convertedAmount);
-    };
-  });
+      await performConversion(user, to, convertedAmount)
+    }
+  })
 
-  it("Should error if you do not convert the amount first", () => {
+  it('Should error if you do not convert the amount first', () => {
     const handleConversionRequest = async (
       user: User,
       from: string,
@@ -51,24 +49,24 @@ describe("Possible implementations", () => {
       amount: number,
     ) => {
       // @ts-expect-error
-      const authorizedUser = ensureUserCanConvert(user, amount);
+      const authorizedUser = ensureUserCanConvert(user, amount)
 
       // @ts-expect-error
-      await performConversion(authorizedUser, to, amount);
-    };
-  });
+      await performConversion(authorizedUser, to, amount)
+    }
+  })
 
-  it("Should pass type checking if you authorize the user AND convert the amount", () => {
+  it('Should pass type checking if you authorize the user AND convert the amount', () => {
     const handleConversionRequest = async (
       user: User,
       from: string,
       to: string,
       amount: number,
     ) => {
-      const convertedAmount = await getConversionRateFromApi(amount, from, to);
-      const authorizedUser = ensureUserCanConvert(user, convertedAmount);
+      const convertedAmount = await getConversionRateFromApi(amount, from, to)
+      const authorizedUser = ensureUserCanConvert(user, convertedAmount)
 
-      await performConversion(authorizedUser, to, convertedAmount);
-    };
-  });
-});
+      await performConversion(authorizedUser, to, convertedAmount)
+    }
+  })
+})
--- src/01-branded-types/06-index-signatures.problem.ts
+++ src/01-branded-types/06-index-signatures.problem.ts
@@ -1,18 +1,18 @@
-import { it } from "vitest";
-import { Brand } from "../helpers/Brand";
-import { Equal, Expect } from "../helpers/type-utils";
+import { it } from 'vitest'
+import { Brand } from '../helpers/Brand'
+import { Equal, Expect } from '../helpers/type-utils'
 
-type PostId = Brand<string, "PostId">;
-type UserId = Brand<string, "UserId">;
+type PostId = Brand<string, 'PostId'>
+type UserId = Brand<string, 'UserId'>
 
 interface User {
-  id: UserId;
-  name: string;
+  id: UserId
+  name: string
 }
 
 interface Post {
-  id: PostId;
-  title: string;
+  id: PostId
+  title: string
 }
 
 /**
@@ -22,40 +22,37 @@ interface Post {
  * You'll need an index signature of some kind - or maybe
  * two!
  */
-const db: Record<string, User | Post> = {};
+const db: Record<UserId, User> & Record<PostId, Post> = {}
 
-it("Should let you add users and posts to the db by their id", () => {
-  const postId = "post_1" as PostId;
-  const userId = "user_1" as UserId;
+it('Should let you add users and posts to the db by their id', () => {
+  const postId = 'post_1' as PostId
+  const userId = 'user_1' as UserId
 
   db[postId] = {
     id: postId,
-    title: "Hello world",
-  };
+    title: 'Hello world',
+  }
 
   db[userId] = {
     id: userId,
-    name: "Miles",
-  };
+    name: 'Miles',
+  }
 
-  const post = db[postId];
-  const user = db[userId];
+  const post = db[postId]
+  const user = db[userId]
 
-  type tests = [
-    Expect<Equal<typeof post, Post>>,
-    Expect<Equal<typeof user, User>>,
-  ];
-});
+  type tests = [Expect<Equal<typeof post, Post>>, Expect<Equal<typeof user, User>>]
+})
 
-it("Should fail if you try to add a user under a post id", () => {
-  const postId = "post_1" as PostId;
-  const userId = "user_1" as UserId;
+it('Should fail if you try to add a user under a post id', () => {
+  const postId = 'post_1' as PostId
+  const userId = 'user_1' as UserId
 
   const user: User = {
     id: userId,
-    name: "Miles",
-  };
+    name: 'Miles',
+  }
 
   // @ts-expect-error
-  db[postId] = user;
-});
+  db[postId] = user
+})
